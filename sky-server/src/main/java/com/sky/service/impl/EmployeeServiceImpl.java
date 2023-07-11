9f9ad4d012359596d62495906858d03b2dc63d63
oyrain <oyrain98@gmail.com>
2023-07-12 00:38:48 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -105,4 +105,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return pageResult;
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder().
+                status(status).
+                id(id).build();
+        employeeMapper.update(employee);
+    }
 }
